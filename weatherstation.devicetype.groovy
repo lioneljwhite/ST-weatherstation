9a7ab3f89ba36556da50be1e32b3985f2d61d43e
yracine <yracine66@gmail.com>
2014-06-05 07:23:12 -0400

    In Celcius....

--- weatherstation.devicetype.groovy
+++ weatherstation.devicetype.groovy
@@ -1,11 +1,11 @@
 /**
+ /**
  *  Weather Station
  *
- *  Author: jnovack@gmail.com
- *  Date: 2013-08-15
- *  Code: https://github.com/smartthings-users/device-type.weatherstation
- *
- * Copyright (C) 2013 Justin J. Novack <jnovack@gmail.com>
+ *  Author: yracine66@gmail.com
+ *  Date: 2014-06-04
+ 
+ * Copyright (C) jnovack@gmail.com
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal in the Software
  * without restriction, including without limitation the rights to use, copy, modify,
@@ -27,11 +27,23 @@ preferences {
 }
 
 metadata {
-    definition (name: "Weather Station", author: "jnovack@gmail.com") {
-      capability "Polling"
+    definition (name: "Weather Station", author: "yracine66@gmail.com") {
       capability "Relative Humidity Measurement"
-      capability "Water Sensor"
       capability "Temperature Measurement"
+      capability "Water Sensor"
+      capability "Polling"
+      
+      attribute "snow", "string"
+      attribute "water", "string"
+      attribute "feels_like", "string"
+      attribute "wind_direction", "string"
+      attribute "wind_speed", "string"
+      attribute "uv_index", "string" 
+      attribute "forecast", "string" 
+      attribute "precipitation", "string" 
+      attribute "location", "string" 
+      
+      
     }
 
     simulator {
@@ -40,27 +52,27 @@ metadata {
 
     tiles {
         // First Row
-        valueTile("temperature", "device.temperature", width: 1, height: 1, canChangeIcon: true) {
-            state("temperature", label: 'Outside\n${currentValue}°F', unit:"F", backgroundColors: [
-                    [value: 31, color: "#153591"],
-                    [value: 44, color: "#1e9cbb"],
-                    [value: 59, color: "#90d2a7"],
-                    [value: 74, color: "#44b621"],
-                    [value: 84, color: "#f1d801"],
-                    [value: 95, color: "#d04e00"],
-                    [value: 96, color: "#bc2323"]
-                ]
+        valueTile("temperature", "device.temperature", width: 1, height: 1, inactiveLabel: false,canChangeIcon: true) {
+            state("temperature", label: 'Outside\n${currentValue}°', unit:"C", backgroundColors: [
+                     [value: 0, color: "#153591"],
+    	             [value: 8, color: "#1e9cbb"],
+                     [value: 14, color: "#90d2a7"],
+            	     [value: 20, color: "#44b621"],
+                     [value: 24, color: "#f1d801"],
+                     [value: 29, color: "#d04e00"],
+                     [value: 36, color: "#bc2323"]           
+                 ]
             )
         }
-        valueTile("humidity", "device.humidity", inactiveLabel: false, decoration: "flat") {
-            state "default",  label:'${currentValue}%', unit: "Humidity"
+        valueTile("humidity", "device.humidity", decoration: "flat",inactiveLabel: false) {
+            state "default",  label:'Humidity\n${currentValue}%', unit: "Humidity"
         }
-        valueTile("feels_like", "device.feels_like", inactiveLabel: false, decoration: "flat") {
-            state "feels_like", label: '${currentValue}ºF', unit: "Feels Like"
+        valueTile("feels_like", "device.feels_like", decoration: "flat",inactiveLabel: false) {
+            state "feels_like", label: 'FeelsLike\n${currentValue}º', unit:"C"
         }
 
         // Second Row
-        standardTile("forecast", "device.forecast", inactiveLabel: false, decoration: "flat") {
+        standardTile("forecast", "device.forecast", decoration: "flat") {
             state "default",        label: 'updating...',        icon: "st.unknown.unknown.unknown"
             state "chanceflurries", label: 'Chance of Flurries', icon: "st.Weather.weather6"
             state "chancerain",     label: 'Chance of Rain',     icon: "st.Weather.weather9"
@@ -68,7 +80,7 @@ metadata {
             state "chancesnow",     label: 'Chance of Snow',     icon: "st.Weather.weather6"
             state "chancetstorms",  label: 'Chance of TStorms',  icon: "st.Weather.weather9"
             state "clear",          label: 'Clear',              icon: "st.Weather.weather14"
-            state "cloudy",         label: 'Cloudy',             icon: "st.Weather.weather15"
+            state "cloudy",         label: 'Cloudy',             icyon: "st.Weather.weather15"
             state "flurries",       label: 'Flurries',           icon: "st.Weather.weather6"
             state "fog",            label: 'Fog',                icon: "st.Weather.weather13"
             state "hazy",           label: 'Hazy',               icon: "st.Weather.weather13"
@@ -82,8 +94,11 @@ metadata {
             state "sunny",          label: 'Sunny',              icon: "st.Weather.weather14"
             state "tstorms",        label: 'Thunder Storms',     icon: "st.Weather.weather10"
         }
-        valueTile("wind_speed", "device.wind_speed", inactiveLabel: false, decoration: "flat") {
-            state "wind_speed", label: '${currentValue}', unit: "mph", backgroundColors: [
+        standardTile("conditions", "device.forecast", decoration: "flat",width: 2, height: 1) {
+            state "default", label: 'Fcast\n${currentValue}'        
+        }
+        valueTile("wind_speed", "device.wind_speed", inactiveLabel: false, decoration: "flat",width: 1, height: 1) {
+            state "wind_speed", label: 'W.Speed\n${currentValue}', unit: "km", backgroundColors: [
                 // Values and colors based on the Beaufort Scale
                 // http://en.wikipedia.org/wiki/Beaufort_scale#Modern_scale
                 [value: 0,  color: "#ffffff"],
@@ -101,21 +116,24 @@ metadata {
                 [value: 74, color: "#ff0000"]
             ]
         }
-        valueTile("wind_direction", "device.wind_direction", inactiveLabel: false, decoration: "flat") {
+        valueTile("wind_direction", "device.wind_direction", inactiveLabel: false, width: 1, height: 1,decoration: "flat") {
             state "wind_direction", label: '${currentValue}', unit: "Direction"
         }
 
         // Third Row
-        valueTile("uv_index", "device.uv_index", inactiveLabel: false, decoration: "flat") {
-            state "uv_index", label: '${currentValue}', unit: "UV Index"
+        valueTile("uv_index", "device.uv_index", decoration: "flat") {
+            state "uv_index", label: '${currentValue}UV', unit: "UV Index"
         }
-        standardTile("water", "device.water", inactiveLabel: false, decoration: "flat") {
+        standardTile("water", "device.water", decoration: "flat",inactiveLabel: false) {
             state "default", label: 'updating...', icon: "st.unknown.unknown.unknown"
             state "true",    label:'Wet',          icon: "st.alarm.water.wet",        backgroundColor:"#ff9999"
             state "false",   label:'Dry',          icon: "st.alarm.water.dry",        backgroundColor:"#99ff99"
         }
-        valueTile("location", "device.location", inactiveLabel: false, decoration: "flat") {
-            state "location", label: '${currentValue}', unit: "ZipCode"
+        valueTile("precipitation", "device.precipitation", inactiveLabel: false, width: 1, height: 1,decoration: "flat") {
+            state "precipitation", label: 'Precip\n ${currentValue}mm', unit: "mm"
+        }
+        valueTile("location", "device.location", inactiveLabel: false, decoration: "flat", width: 1, height: 1) {
+            state "location", label: 'Location\n${currentValue}', unit: "ZipCode"
         }
 
         // Fourth Row
@@ -127,7 +145,7 @@ metadata {
         main "temperature"
 
         // These tiles will be displayed when clicked on the device, in the order listed here.
-        details(["temperature", "humidity", "feels_like", "forecast", "wind_speed", "wind_direction", "uv_index", "water", "location", "refresh"])
+        details(["temperature", "humidity", "feels_like", "forecast","conditions", "wind_speed", "wind_direction", "uv_index", "water", "precipitation", "location", "refresh"])
     }
 }
 
@@ -160,34 +178,90 @@ def poll() {
 
     // Wind
     log.debug( "Wind Speed: ${weather.current_observation.wind_mph} mph")
-    sendEvent( name: "wind_speed", value: weather.current_observation.wind_mph )
+    
+        
+    def scale = getTemperatureScale()
+    if (scale == 'C') { 
+
+        float windSpeed=milesToKm(weather.current_observation.wind_mph.toFloat())
+        def speedFormat= String.format('%2.1f', windSpeed.round(1))
+        sendEvent( name: "wind_speed", value: speedFormat, unit: "kmh" )
+    }
+    else {
+    
+        sendEvent( name: "wind_speed", value: weather.current_observation.wind_mph.toInteger(), unit: "mph" )
+    
+    }
 
     log.debug( "Wind Direction: ${weather.current_observation.wind_dir}" )
     sendEvent( name: "wind_direction", value: weather.current_observation.wind_dir )
-
     // Set the tiles
-    log.debug( "Current Temperature: ${weather.current_observation.temp_f}ºF" )
-    sendEvent( name: 'temperature', value: weather.current_observation.temp_f )
 
+    log.debug( "Current Temperature: ${weather.current_observation.temp_f}ºF" )
+    if (scale == 'C') { 
+
+        def currentTemp = String.format('%2.1f', fToC(weather.current_observation.temp_f))
+        sendEvent( name: 'temperature', value: currentTemp, unit: "C")
+    }
+    else {
+        sendEvent( name: 'temperature', value: weather.current_observation.temp_f, unit: "F")
+    }
+    
     // Sending a value to a valueTile REQUIRES an Integer (on Android, for now)
     log.debug( "Relative Humidity: ${weather.current_observation.relative_humidity.tokenize('%')[0].toInteger()}" )
-    sendEvent( name: 'humidity', value: weather.current_observation.relative_humidity.tokenize('%')[0].toInteger() )
+    sendEvent( name: 'humidity', value: weather.current_observation.relative_humidity.tokenize('%')[0].toInteger(),
+        unit: "%")
 
+//    log.debug( "Feels Like: ${weather.current_observation.feelslike_f}" )
     log.debug( "Feels Like: ${weather.current_observation.feelslike_f}" )
-    sendEvent( name: 'feels_like', value: weather.current_observation.feelslike_f )
+    
+    if (scale == 'C') { 
+     
+        float feelTemp_c = fToC(weather.current_observation.feelslike_f.toFloat())
 
+        def feelTempFormat =  String.format('%2.1f', feelTemp_c.round(1))
+        sendEvent( name: 'feels_like', value: feelTempFormat, unit: "C")
+    }
+    else {
+
+        sendEvent( name: 'feels_like', value: weather.current_observation.feelslike_f )
+    }
+    
     // Something that looks like an integer will get converted to an integer
+    
     log.debug( "Location: ${weather.current_observation.display_location.zip.toString()}" )
     sendEvent( name: 'location', value: weather.current_observation.display_location.zip.toString())
 
     // Since precip_1hr_in is a string, we need to convert it to a decimal in order to compare it as a number.
-    if (weather.current_observation.precip_1hr_in.toFloat() > 0) {
-        log.debug( "Precipitation: ${weather.current_observation.precip_1hr_in}" )
-        sendEvent( name: 'water', value: "true" )
-    } else {
+    log.debug( "Precipitation: ${precip}" )
+    
+    if (scale == 'C') { 
+     
+
+        float precip = inchToMm(weather.current_observation.precip_1hr_in.toFloat())
+        def precipFormat =  String.format('%2.1f', precip.round(1))
+        sendEvent( name: 'precipitation', value: precipFormat, unit: "mm" )
+    }
+    else {
+    
+        sendEvent( name: 'precipitation', value: weather.current_observation.precip_1hr_in, unit: "inches" )
+    }
+    if (precip > 0) {
+            
+        if (currentTemp < 0) {
+        
+            sendEvent( name: 'snow', value: "true" )
+        }
+        else {
+            sendEvent( name: 'water', value: "true" )
+                
+        }
+        
+     } else {
         log.debug( "Precipitation: None" )
         sendEvent( name: 'water', value: "false" )
-    }
+        sendEvent( name: 'snow', value: "false" )
+    }    
 }
 
 def cToF(temp) {
@@ -196,4 +270,14 @@ def cToF(temp) {
 
 def fToC(temp) {
     return (temp - 32) / 1.8
+}
+
+def milesToKm(distance) {
+    return (distance * 1.609344)
+}
+
+def inchToMm(len) {
+    return (len * 25.400)
+
+
 }
